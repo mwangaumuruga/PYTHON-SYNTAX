eb522950b2305773e9161e1042c3605253deec34
mwangaumuruga <mosesmwangaumuruga@gmail.com>
2025-07-21 22:19:08 +0300

    lvl3

---
+++ testEnv.py
@@ -0,0 +1,5 @@
+#They help produce a series of items which can be manipulated on the user need
+ EPL_TEAMS=["manchester united ","manchester city","chelsea","arsenal","tottenham"]
+ print(EPL_TEAMS)
+ EPL_TEAMS.append("wolves")
+print(EPL_TEAMS)
