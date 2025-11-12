ea7e82dca30c0b7f34f464d2e945281b14b5496d
mwangaumuruga <119576330+mwangaumuruga@users.noreply.github.com>
2025-11-12 19:46:56 +0000

    Fix spelling and grammar in module description
    Corrected spelling and grammatical errors in the module description.

--- 7-BASIC PYTHON SYNTAX(modules).py
+++ 7-BASIC PYTHON SYNTAX(modules).py
@@ -1,3 +1,5 @@
+
+
 """ MODULES are usually imported and generally they are a set of prewrittten code that helps reduce the work of 
 the work of rewriting codes that can be used i.e Maths standards  or a code generate randomness and this helps save 
 time and also get us on pint without focusing on the complex stuff"""
