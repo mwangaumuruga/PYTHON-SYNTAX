864abd7fbb18d090bbb4ebebca389e40b7e93475
mwangaumuruga <119576330+mwangaumuruga@users.noreply.github.com>
2025-11-13 10:36:03 +0000

    Fix typos and add example for random module
    Corrected typos and improved the explanation of modules. Added an example use case for the random module.

--- 7-BASIC PYTHON SYNTAX(modules).py
+++ 7-BASIC PYTHON SYNTAX(modules).py
@@ -53,7 +53,7 @@
 """  are usually imported and generally they are a set of prewrittten code that helps reduce the work of 
 the work of rewriting codes that can be used i.e Maths standards  or a code generate randomness and this helps save 
 time and also get us on pint without focusing on the complex stuff"""
-
+example use case THE RANDOM MODULE
 # A program to randomly  choose a country from a list of must visit countries
 import random
 countries=("rwanda","mauritius","seychelles","southafrica","madagascar""tunisia","turkey","israel","usa","china","spain")
